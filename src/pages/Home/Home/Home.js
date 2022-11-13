478f69d480900ed6fed35f00899697b2dee10225
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-14 00:48:57 +0600

    71-8 testimonial avatar

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -3,6 +3,7 @@ import Banner from '../Banner/Banner';
 import InfoCards from '../InfoCard/InfoCards';
 import MakeAppoinment from '../MakeAppoinment/MakeAppoinment';
 import Services from '../Services/Services';
+import Testimonial from '../Testimonial/Testimonial';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <InfoCards></InfoCards>
             <Services></Services>
             <MakeAppoinment></MakeAppoinment>
+            <Testimonial></Testimonial>
         </div>
     );
 };
