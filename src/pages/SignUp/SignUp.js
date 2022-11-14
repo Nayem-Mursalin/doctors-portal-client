f9d25e6cf8ee25afaa80bcea133bc49260962878
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-14 18:10:27 +0600

    73-5 regex

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -35,7 +35,8 @@ const SignUp = () => {
                         <label className='label'> <span className='label-text'>Password</span></label>
                         <input type="password" {...register("password", {
                             required: "Password is required",
-                            minLength: { value: 6, message: "Password must be 6 character or longer" }
+                            minLength: { value: 6, message: "Password must be 6 character or longer" },
+                            pattern: { value: /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9])/, message: "Password Must be Strong" }
                         })}
                             className="input input-bordered w-full max-w-xs" />
                         {errors.password && <p className='text-red-500'>{errors.password.message}</p>}
