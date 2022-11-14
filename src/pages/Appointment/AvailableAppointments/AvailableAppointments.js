12de85ac01092f43c98754950ab368bc62b120ff
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-14 13:25:21 +0600

    72-5 modal

--- src/pages/Appointment/AvailableAppointments/AvailableAppointments.js
+++ src/pages/Appointment/AvailableAppointments/AvailableAppointments.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import { format } from 'date-fns';
 import AppointmentOption from '../AvailableAppointments/AppointmentOption';
+import BookingModal from '../BookingModal/BookingModal';
 
 const Availableappointments = ({ selectedDate }) => {
     const [appointmentOptions, setAppointmentOptions] = useState([]);
+    const [treatment, setTreatment] = useState(null);
 
     useEffect(() => {
         fetch('appointmentOptions.json')
@@ -19,9 +21,16 @@ const Availableappointments = ({ selectedDate }) => {
                     appointmentOptions.map(option => <AppointmentOption
                         key={option._id}
                         appointmentOption={option}
+                        setTreatment={setTreatment}
                     ></AppointmentOption>)
                 }
             </div>
+            {
+                treatment &&
+                <BookingModal
+                    treatment={treatment}
+                ></BookingModal>
+            }
         </section>
     );
 };
