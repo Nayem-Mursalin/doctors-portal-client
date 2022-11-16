c5cb4364881b4803118367ac4ac2f7e4035eaccf
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-16 19:27:59 +0600

    74-4 booking data

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,27 +1,29 @@
 import toast from 'react-hot-toast';
 import React, { useContext, useState } from 'react';
 import { useForm } from "react-hook-form";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthProvider';
 
 const SignUp = () => {
     const { register, formState: { errors }, handleSubmit } = useForm();
     const { createUser, updateUser } = useContext(AuthContext);
     const [signUpError, setSignUpError] = useState('');
+    const navigate = useNavigate();
 
     const handleSignUp = (data) => {
-        console.log(data);
         setSignUpError('');
         createUser(data.email, data.password)
             .then(res => {
                 const user = res.user;
                 console.log(user)
-                toast('')
+                toast('User Created Successfully')
                 const userInfo = {
                     displayName: data.name
                 }
                 updateUser(userInfo)
-                    .then(() => { })
+                    .then(() => {
+                        navigate('/');
+                    })
                     .catch(error => console.log(error))
             })
             .catch(error => {
