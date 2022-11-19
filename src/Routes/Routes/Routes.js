19f155d9ca79ecf8aba6edd7e1c86bd966f9fc9a
Nayem-Mursalin <nayem.mursalin19@gmail.com>
2022-11-20 04:05:58 +0600

    77-3 error elememt & stripe install

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -9,6 +9,7 @@ import Payment from "../../pages/DashBoard/DashBoard/Payment/Payment";
 import ManageDoctors from "../../pages/DashBoard/ManageDoctors/ManageDoctors";
 import Home from "../../pages/Home/Home/Home";
 import Login from "../../pages/Login/Login";
+import DisplayError from "../../pages/Shared/DisplayError/DisplayError";
 import SignUp from "../../pages/SignUp/SignUp";
 import AdminRoute from "../AdminRoute/AdminRoute";
 import PrivateRoute from '../PrivateRoute/PrivateRoute';
@@ -17,6 +18,7 @@ export const router = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
+        errorElement: <DisplayError></DisplayError>,
         children: [
             {
                 path: '/',
@@ -40,6 +42,7 @@ export const router = createBrowserRouter([
     {
         path: '/dashboard',
         element: <PrivateRoute><DashboardLayout></DashboardLayout></PrivateRoute>,
+        errorElement: <DisplayError></DisplayError>,
         children: [
             {
                 path: '/dashboard',
--- src/pages/DashBoard/DashBoard/Payment/Payment.js
+++ src/pages/DashBoard/DashBoard/Payment/Payment.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Payment = () => {
-    const { data } = useLoaderData();
+    const booking = useLoaderData();
+    const { treatement, price, appointmentDate, slot } = booking;
+
     return (
         <div>
-            <h3 className="text-3xl">Payment</h3>
+            <h3 className="text-3xl">Payment for {treatement}</h3>
+            <p className='text-3xl'>Please pay <strong>${price}</strong> for your appoinment on {appointmentDate} at {slot}</p>
         </div>
     );
 };
